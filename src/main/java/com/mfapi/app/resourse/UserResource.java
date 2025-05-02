2f86a7b773b54deaab88671eb72ff2a86fc5ef70
MatusMoura2 <matus.moura333x2@gmail.com>
2025-05-02 00:55:26 -0300

    implementçao do ModelMapper, implementaçao do padrao DTO

--- src/main/java/com/mfapi/app/resourse/UserResource.java
+++ src/main/java/com/mfapi/app/resourse/UserResource.java
@@ -1,5 +1,6 @@
 package com.mfapi.app.resourse;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mfapi.app.domain.User;
+import com.mfapi.app.domain.dto.UserDto;
 import com.mfapi.app.servises.UserServise;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
-
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Autowired
 	private UserServise userServise;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(userServise.findById(id));
+	public ResponseEntity<UserDto> findById(@PathVariable Integer id){
+		return ResponseEntity.ok().body(modelMapper.map(userServise.findById(id), UserDto.class));
 	}
 }
